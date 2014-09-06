ef9e4d9541ae73ff2d961ce53d4d587e58449b60
Jerry Kiely <jerry@cowboysmall.com>
2014-09-05 23:02:40 -0400

    added a comment explaining the data pre-processing

--- exploratory-data-analysis/project1/plot4.R
+++ exploratory-data-analysis/project1/plot4.R
@@ -1,3 +1,9 @@
+# I pre-processed the data with the following command:
+# 
+#   sed -n -e 1p -e 66638,69517p household_power_consumption.txt > household_power_consumption_truncated.txt
+# 
+# the purpose was to extract the relevant entries.
+# The pre-processed file is included for convenience
 
 data <- read.table("./data/household_power_consumption_truncated.txt", header = TRUE, sep = ";");
 
